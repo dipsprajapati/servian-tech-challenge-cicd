264e07c3518356f87a54bd69bcab368c7110be53
dipsprajapati <dips.p.007@gmail.com>
2020-10-07 12:51:42 +0800

    variable.tf and main.tf updated

--- part01/iac_gke_cluster/variables.tf
+++ part01/iac_gke_cluster/variables.tf
@@ -1,21 +1,21 @@
 variable "project" {
-  default = "cicd-workshops"
+  default = "servian-tech-challenge"
 }
 
 variable "region" {
-  default = "us-east1"
+  default = "us-central1"
 }
 
 variable "zone" {
-  default = "us-east1-d"
+  default = "us-central1-a"
 }
 
 variable "cluster" {
-  default = "cicd-workshops"
+  default = "servian-app-v2"
 }
 
 variable "credentials" {
-  default = "~/.ssh/cicd_demo_gcp_creds.json"
+  default = "/Users/dipesh/mydocker/servian-tech-challenge-527f3d89ef90.json"
 }
 
 variable "kubernetes_min_ver" {
@@ -27,9 +27,9 @@ variable "kubernetes_max_ver" {
 }
 
 variable "machine_type" {
-  default = "g1-small"
+  default = "e2-micro"
 }
 
 variable "app_name" {
-  default = "cicd-101"
+  default = "servian-app-v2"
 }
