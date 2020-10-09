863862a42241195b7cfbd373b3d26ad723aba9b9
dipsprajapati <dips.p.007@gmail.com>
2020-10-09 15:02:32 +0800

    deploy.sh corrected

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
-docker build -t dipsprajapati/serviantechchallenge-app:latest -t dipsprajapati/serviantechchallenge-app:$SHA -f ./TechChallengeAppResponse/TechChallengeApp/Dockerfile ./TechChallengeAppResponse/TechChallengeApp
+docker build -t dipsprajapati/serviantechchallenge-app:latest -f ./TechChallengeAppResponse/TechChallengeApp/Dockerfile ./TechChallengeAppResponse/TechChallengeApp
 
 docker push dipsprajapati/serviantechchallenge-app:latest
 
-docker push dipsprajapati/serviantechchallenge-app:$SHA
+#docker push dipsprajapati/serviantechchallenge-app:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/app-deployment app=dipsprajapati/serviantechchallenge-app:$SHA
